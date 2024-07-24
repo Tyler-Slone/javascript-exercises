9e5f9f3999d11b980004741551c752a795d3ba2e
Tyler Slone <tyl3rslon3@gmail.com>
2024-07-24 12:27:17 -0400

    Pass initial test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, remove) {
+    if (array.includes(remove)){
+
+        // Find index of argument to remove
+        let index = array.indexOf(remove);
+
+        // Remove argument at index
+        array.splice(index, 1);
+        
+        return array;
+    } else {
+        return 'Argument not found.';
+    }
 };
 
 // Do not edit below this line

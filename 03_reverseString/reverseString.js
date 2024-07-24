f6ae44b5bbb91264330e8679aabb284c10ea25ba
Tyler Slone <tyl3rslon3@gmail.com>
2024-07-23 22:35:37 -0400

    Pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    string = string.split('');
+    string = string.reverse();
+    let reverseString = string.join('');
+    return reverseString;
 };
 
 // Do not edit below this line

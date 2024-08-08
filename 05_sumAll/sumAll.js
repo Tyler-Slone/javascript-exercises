08db1d35f1189c594f3f26ddb88c878510a81cab
Tyler Slone <tyl3rslon3@gmail.com>
2024-08-08 09:18:39 -0400

    Pass test 1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    // Error handling
+    if (min < 0 || max < 0){
+        return 'ERROR'
+    };
 
+    // if (){
+    //     return 'ERROR'
+    // }
+
+    // Fix swapped min and max values
+    if (min > max){
+        max = max + min;
+        min = max - min;
+        max = max - min;
+    };
+
+    let sum = 0
+    for (let i = min; i <= max; i++) {
+        sum += i
+    };
+
+    return sum;
 };
 
 // Do not edit below this line

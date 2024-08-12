792a9a5ed552db3eadc2ca0238cf8de2a9f0b2ab
Tyler Slone <tyl3rslon3@gmail.com>
2024-08-12 17:00:02 -0400

    Pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+// Formula (°F − 32) × 5/9 = °C
+const convertToCelsius = function(tempFahrenheit) {
+  let convertedTempCelsius = (tempFahrenheit - 32) * 5/9;
+  convertedTempCelsius = Math.round(convertedTempCelsius * 10) / 10
+  return convertedTempCelsius;
 };
 
-const convertToFahrenheit = function() {
+// Formula (°C × 9/5) + 32 = °F
+const convertToFahrenheit = function(tempCelsius) {
+  let convertedTempFahrenheit = (tempCelsius * 9/5) + 32;
+  convertedTempFahrenheit = Math.round(convertedTempFahrenheit * 10) / 10
+  return convertedTempFahrenheit;
 };
 
 // Do not edit below this line

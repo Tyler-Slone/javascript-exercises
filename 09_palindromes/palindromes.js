bd8cb54bad7c490243369e71660e971f914adc4e
Tyler Slone <tyl3rslon3@gmail.com>
2024-09-11 15:48:19 -0400

    Pass all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const clean = string
+    .toLowerCase()
+    .split("")
+    .filer((letter) => alpha.includes(character))
+    .join("");
 
+  const reverseClean = clean.split("").reverse().join("");
+
+  return clean === reverseClean;
 };
 
 // Do not edit below this line

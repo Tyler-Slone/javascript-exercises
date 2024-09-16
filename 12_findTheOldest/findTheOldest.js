fc29ce9b1417abd6211b9bebed9bba50e17b92cb
Tyler Slone <tyl3rslon3@gmail.com>
2024-09-15 22:58:04 -0400

    Pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const age = function(birth, death) {
+    if (!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
+const findTheOldest = function (people) {
+   return people.reduce((oldest, current) => {
+    const oldestAge = age(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = age(current.yearOfBirth, current.yearOfDeath);
+    return oldestAge < currentAge ? current : oldest;
+   }); 
+};
 // Do not edit below this line
 module.exports = findTheOldest;
